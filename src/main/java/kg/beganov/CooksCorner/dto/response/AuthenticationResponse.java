b1f57747d0f951b92e469fff1aa8ae17f09c90ca
asadaravani <abegano1@stu.vistula.edu.pl>
2024-05-22 08:32:51 +0600

    updateForLogin

--- src/main/java/kg/beganov/CooksCorner/dto/response/AuthenticationResponse.java
+++ src/main/java/kg/beganov/CooksCorner/dto/response/AuthenticationResponse.java
@@ -2,13 +2,18 @@ package kg.beganov.CooksCorner.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthenticationResponse {
     @JsonProperty
-    private String token;
+    String token;
+    Long userId;
+    String userName;
+
 }
